98c1547fe0e66765b11c20d0876f2ef3aae93d8f
Vineeth Sirikonda <vineethsirikonda736@gmail.com>
2026-07-28 11:46:55 +0530

    Create Login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,7 @@
+import Login from "./pages/Login";
+
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900">
-      <h1 className="text-5xl font-bold text-cyan-400">
-        CodeTrack AI 🚀
-      </h1>
-    </div>
-  );
+  return <Login />;
 }
 
 export default App;
